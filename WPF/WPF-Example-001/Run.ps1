6280ccf44f732be225b72c99ebf4f7f36481390d
Marcus <marcusaclayton@gmail.com>
2016-12-22 15:53:10 -0800

    Added PS run script, and updated tabs.

--- WPF/WPF-Example-001/Run.ps1
+++ WPF/WPF-Example-001/Run.ps1
@@ -1,4 +1,6 @@
-﻿<Window x:Class="WPF_Example_001.MainWindow"
+﻿    #ERASE ALL THIS AND PUT XAML BELOW between the @" "@ 
+$inputXML = @"
+<Window x:Class="WPF_Example_001.MainWindow"
         xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation"
         xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml"
         xmlns:d="http://schemas.microsoft.com/expression/blend/2008"
@@ -85,3 +87,134 @@
 
     </Grid>
 </Window>
+
+"@ 
+
+$inputXML = $inputXML -replace 'mc:Ignorable="d"','' -replace "x:N",'N' -replace '^<Win.*', '<Window'
+[void][System.Reflection.Assembly]::LoadWithPartialName('presentationframework')
+[xml]$XAML = $inputXML
+#Read XAML
+
+    $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
+  try{$Form=[Windows.Markup.XamlReader]::Load( $reader )}
+catch [System.Management.Automation.MethodInvocationException] {
+    Write-Warning "We ran into a problem with the XAML code.  Check the syntax for this control..."
+    write-host $error[0].Exception.Message -ForegroundColor Red
+    if ($error[0].Exception.Message -like "*button*"){
+        write-warning "Ensure your &lt;button in the `$inputXML does NOT have a Click=ButtonClick property.  PS can't handle this`n`n`n`n"}
+}
+catch{#if it broke some other way :D
+    Write-Host "Unable to load Windows.Markup.XamlReader. Double-check syntax and ensure .net is installed."
+        }
+
+#===========================================================================
+# Store Form Objects In PowerShell
+#===========================================================================
+
+$xaml.SelectNodes("//*[@Name]") | %{Set-Variable -Name "WPF$($_.Name)" -Value $Form.FindName($_.Name)}
+
+Function Get-FormVariables{
+if ($global:ReadmeDisplay -ne $true){Write-host "If you need to reference this display again, run Get-FormVariables" -ForegroundColor Yellow;$global:ReadmeDisplay=$true}
+write-host "Found the following interactable elements from our form" -ForegroundColor Cyan
+get-variable WPF*
+}
+
+Get-FormVariables
+
+#===========================================================================
+# Use this space to add code to the various form elements in your GUI
+#===========================================================================
+                                                                   
+$Form.Add_Loaded({$WPFComputerName.Text = $env:COMPUTERNAME})    
+
+$WPFVerify_button.Add_Click({if (Test-Connection $WPFComputerName.Text -Count 1 -Quiet){
+    write-host "$($WPFComputerName.Text ) responded, unlocking"
+    $WPFtabControl.Items[1..3] | % {$_.IsEnabled = $true}
+    }
+    else{
+    write-host "$($WPFComputerName.Text ) did not respond, staying locked"
+    $WPFtabControl.Items[1..3] | % {$_.IsEnabled = $false}
+    }
+})
+
+$WPFVerify_button.add_MouseEnter({Write-Host "Mouse On."})
+$WPFVerify_button.add_MouseLeave({Write-Host "Mouse Off."})
+
+$WPFLoad_diskinfo_button.Add_Click({
+Function Get-DiskInfo {
+param($computername =$env:COMPUTERNAME)
+ 
+Get-WMIObject Win32_logicaldisk -ComputerName $computername | Select-Object @{Name='ComputerName';Ex={$computername}},`
+                                                                    @{Name=‘Drive Letter‘;Expression={$_.DeviceID}},`
+                                                                    @{Name=‘Drive Label’;Expression={$_.VolumeName}},`
+                                                                    @{Name=‘Size(MB)’;Expression={[int]($_.Size / 1MB)}},`
+                                                                    @{Name=‘FreeSpace%’;Expression={[math]::Round($_.FreeSpace / $_.Size,2)*100}}
+                                                                 }
+                                                                  
+
+Get-DiskInfo -computername $WPFComputerName.Text | % {$WPFdisk_listView.AddChild($_)}
+$WPFDiskLabel.Content = "Disk info for system $($WPFComputerName.Text)"
+
+})
+
+$WPFLoad_services.Add_Click({
+    $WPFservice_listView.Items.Clear()
+    get-service -ComputerName $WPFComputerName.Text | % {$WPFservice_listView.AddChild($_)}
+    })
+
+$WPFStart_service.Add_Click({
+    $WPFservice_listView.SelectedItems | % {
+        write-host "Starting $($_.ServiceName) on $($WPFComputerName.Text)"
+        ($stat = gwmi Win32_Service -ComputerName $WPFComputerName.Text | ? Name -like $_.ServiceName).StartService()
+            if ($stat -eq 0){write-host "--Success"}
+            elseif ($stat -eq 2){
+            write-host "--need elevated perms"
+            }
+            else{
+            write-host "--error"
+            }
+
+        }
+
+    write-host "Refreshing Services on $($WPFComputerName.Text)"
+     $WPFservice_listView.Items.Clear()
+    get-service -ComputerName $WPFComputerName.Text | % {$WPFservice_listView.AddChild($_)}
+})
+
+$WPFStop_service.Add_Click({
+    $WPFservice_listView.SelectedItems | % {
+        write-host "Stopping $($_.ServiceName) on $($WPFComputerName.Text)"
+        ($stat = gwmi Win32_Service -ComputerName $WPFComputerName.Text | ? Name -like $_.ServiceName).StopService()
+            if ($stat -eq 0){write-host "--Success"}
+            elseif ($stat -eq 2){
+            write-host "--need elevated perms"
+            }
+            else{
+            write-host "--error"
+            }
+
+        }
+
+    write-host "Refreshing Services on $($WPFComputerName.Text)"
+     $WPFservice_listView.Items.Clear()
+    get-service -ComputerName $WPFComputerName.Text | % {$WPFservice_listView.AddChild($_)}
+})
+
+
+
+#Adding items to a dropdown/combo box
+    #$vmpicklistView.items.Add([pscustomobject]@{'VMName'=($_).Name;Status=$_.Status;Other="Yes"})
+    
+#Setting the text of a text box to the current PC name    
+    #$WPFtextBox.Text = $env:COMPUTERNAME
+    
+#Adding code to a button, so that when clicked, it pings a system
+# $WPFbutton.Add_Click({ Test-connection -count 1 -ComputerName $WPFtextBox.Text
+# })
+#===========================================================================
+# Shows the form
+#===========================================================================
+write-host "To show the form, run the following" -ForegroundColor Cyan
+'$Form.ShowDialog() | out-null'
+
+$Form.ShowDialog() | out-null
