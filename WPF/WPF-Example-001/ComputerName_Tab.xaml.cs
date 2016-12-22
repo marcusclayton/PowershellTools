d1c1fd1e1b945c4c828bf36a0879a81500c97b4c
Marcus <marcusaclayton@gmail.com>
2016-12-22 13:39:42 -0800

    Added Welcome and Tab-Activity-1 tabs.

---
+++ WPF/WPF-Example-001/ComputerName_Tab.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_Example_001
+{
+    /// <summary>
+    /// Interaction logic for ComputerName_Tab.xaml
+    /// </summary>
+    public partial class ComputerName_Tab : Window
+    {
+        public ComputerName_Tab()
+        {
+            InitializeComponent();
+        }
+    }
+}
